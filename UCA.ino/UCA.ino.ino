45e5f9f64fd6f540003a1cd29aca70487752d60c
GKM090503 <72422431+NormalHuman-Anything@users.noreply.github.com>
2022-07-16 11:53:03 +0900

    LED 3개 껐다 키기

--- UCA.ino/UCA.ino.ino
+++ UCA.ino/UCA.ino.ino
@@ -1,10 +1,20 @@
 void setup() {
  pinMode(7, OUTPUT);
+ pinMode(6, OUTPUT);
+ pinMode(5, OUTPUT);
 }
 
 void loop() {
   digitalWrite(7, HIGH);
   delay(1000);
   digitalWrite(7, LOW);
+  delay(1000);
+  digitalWrite(6, HIGH);
+  delay(1000);
+  digitalWrite(6, LOW);
+  delay(1000);
+  digitalWrite(5, HIGH);
+  delay(1000);
+  digitalWrite(5, LOW);
   delay(1000);
 }
